9c15aa5b2a7971051615d3a38249b58e15921e3a
Stanislav Kozachenko <stanislav.03@list.ru>
2026-05-05 11:42:35 +0300

    feat: add ConfigurationExtensions

---
+++ src/GitHubProfileAnalytics/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,7 @@
+namespace GitHubProfileAnalytics.Extensions;
+
+public static class ConfigurationExtensions
+{
+    public static string GetRequired(this IConfiguration configuration, string key) =>
+        configuration[key] ?? throw new InvalidOperationException($"{key} is not configured");
+}
