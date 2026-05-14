4743c3ab72550c55d98003a28671109100eba8ca
Stanislav Kozachenko <stanislav.03@list.ru>
2026-05-14 16:59:17 +0300

    refactor: make domain entities immutable using constructor injection

--- src/GitHubProfileAnalytics/Domain/SearchHistory.cs
+++ src/GitHubProfileAnalytics/Domain/SearchHistory.cs
@@ -1,9 +1,14 @@
 namespace GitHubProfileAnalytics.Domain;
 
-public class SearchHistory
+public class SearchHistory(
+    Guid id,
+    Guid userId,
+    string gitHubUserName,
+    DateTimeOffset searchedAt
+)
 {
-    public Guid Id { get; set; }
-    public Guid UserId { get; set; }
-    public string GitHubUserName { get; set; } = string.Empty;
-    public DateTimeOffset SearchedAt { get; set; }
+    public Guid Id { get; init; } = id;
+    public Guid UserId { get; init; } = userId;
+    public string GitHubUserName { get; init; } = gitHubUserName;
+    public DateTimeOffset SearchedAt { get; init; } = searchedAt;
 }
--- src/GitHubProfileAnalytics/Domain/User.cs
+++ src/GitHubProfileAnalytics/Domain/User.cs
@@ -1,11 +1,10 @@
 namespace GitHubProfileAnalytics.Domain;
 
-public class User
+public class User(Guid id, string email, string passwordHash, DateTimeOffset createdAt)
 {
-    public Guid Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public DateTimeOffset CreatedAt { get; set; }
-
-    public ICollection<RefreshToken> RefreshTokens { get; set; } = [];
+    public Guid Id { get; init; } = id;
+    public string Email { get; init; } = email;
+    public string PasswordHash { get; init; } = passwordHash;
+    public DateTimeOffset CreatedAt { get; init; } = createdAt;
+    public ICollection<RefreshToken> RefreshTokens { get; } = [];
 }
