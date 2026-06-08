763c11c08f25d94bbc4e2c439987910e3c12a3c2
Stanislav Kozachenko <49488521+StanislavKozachenko@users.noreply.github.com>
2026-06-08 13:47:28 +0300

    feat: add winner field to comparison response (#64)
    * feat: add winner field to comparison response
    
    * fix: revert CreateSut to single-arg constructor on winner branch

--- src/GitHubProfileAnalytics/DTOs/Analytics/ProfileComparisonDto.cs
+++ src/GitHubProfileAnalytics/DTOs/Analytics/ProfileComparisonDto.cs
@@ -3,4 +3,10 @@ namespace GitHubProfileAnalytics.DTOs.Analytics;
 public class ProfileComparisonDto(IReadOnlyList<ComparisonEntryDto> profiles)
 {
     public IReadOnlyList<ComparisonEntryDto> Profiles { get; init; } = profiles;
+    public string? Winner { get; init; } =
+        profiles.Count == 2 && profiles[0].Score != profiles[1].Score
+            ? profiles[0].Score > profiles[1].Score
+                ? profiles[0].Username
+                : profiles[1].Username
+            : null;
 }
